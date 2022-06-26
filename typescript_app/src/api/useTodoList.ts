969a7e8f567d3521f5e95213ca47b0cbf31db4d7
yasudamakoto <hare.v1v1enne@gmail.com>
2022-06-26 16:45:41 +0900

    useQueryの別パターンを確認

--- typescript_app/src/api/useTodoList.ts
+++ typescript_app/src/api/useTodoList.ts
@@ -1,6 +1,6 @@
 import {
   useQuery
-} from 'react-query'
+} from 'react-query';
 
 type TodoType = {
   userId: Number,
@@ -9,24 +9,51 @@ type TodoType = {
   completed: boolean,
 }
 
-const fetchTodosQueryFn = async () =>
-  await fetch('https://jsonplaceholder.typicode.com/todos').then(res => {
-    return res.json();
+const fetchTodosQueryFn = async () => {
+  const result = await fetch('https://jsonplaceholder.typicode.com/todos');
+
+  if (result.status !== 200) {
+    throw new Error('fetch Error')
   }
-  );
+  return result.json();
+}
+
+const fetchCompletedTodosQueryFn = async () => {
+  const result = await fetch('https://jsonplaceholder.typicode.com/todos?completed=true');
+
+  if (result.status !== 200) {
+    throw new Error('fetch Error')
+  }
+  return result.json();
+}
 
 // TODO:要確認
 // React Hook "useQuery" is called in function "fetchTodoList" that is neither a React function component nor a custom React Hook function. React component names must start with an uppercase letter. React Hook names must start with the word "use"  react-hooks/rules-of-hooks
 export const useTodoList = () => {
   const fetchTodoList = () => {
     console.log('fetch todos');
+
+    // return useQuery<TodoType[], Error>(
+    //   'todos',
+    //   // ['todos', { completed: true }],
+    //   fetchTodosQueryFn,
+    //   {
+    //     notifyOnChangeProps: ["data"],
+    //     refetchOnWindowFocus: false,
+    //   }
+    // );
+
+    // オブジェクトとして書くことも可能
     return useQuery<TodoType[], Error>(
-      'todos',
-      fetchTodosQueryFn,
       {
-        refetchOnWindowFocus: false,
+        queryKey: 'todos',
+        queryFn: fetchTodosQueryFn,
+        ...{
+          notifyOnChangeProps: ["data"],
+          refetchOnWindowFocus: false,
+        }
       }
-    )
+    );
   }
 
   return { fetchTodoList };
